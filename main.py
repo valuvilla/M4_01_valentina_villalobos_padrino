89fcd2841aea38f7e7b3fcf96cd71548519c04e2
valuvilla <113980834+valuvilla@users.noreply.github.com>
2022-10-08 12:04:16 +0000

    ej2

--- main.py
+++ main.py
@@ -25,16 +25,19 @@ print(b)
 b[:2]=[]
 print(b)
 ##################################
+#Creamos un set encerrando su elementos entre corchetes y mostramos su valor
 numeros = {1,2,3,4}
 print(numeros, "pertence a ", type(numeros))
+#Creamos un dicionario encerrandos sus conjuntos clave:valor entre corchetes y mostramos su valor.
 diccionario = {'Nombre':'valentina','apellido':'villalobos','edad': 16,'año':2004,}
 print(diccionario, "pertence a ", type(diccionario))
+#Mostrar valor
+print(diccionario.get('apellido'))
+#En el set es imposible accder a un valor ya que sus elementos no estan ordenados
 #Modifica diccionario
-print(diccionario['apellido'])
 diccionario['edad']=17
 print(diccionario)
-#intento
-  
+#En el set es imposible modificar un valor
 #Tamaño del diccionario y del set
 print("el set tiene un tamaño de ",len(numeros), " elementos")
 print("el diccionario tiene un tamaño de ",len(diccionario), " elementos")
